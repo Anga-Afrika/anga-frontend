e827a61fef5d459e7d536b95bdc7b19561a963f1
emuiga <muigastephen14@gmail.com>
2024-02-14 01:24:33 +0300

    genesis commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,6 +11,26 @@ const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full'
   },
+  {
+    path: 'login',
+    loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
+  },
+  {
+    path: 'forgot-password',
+    loadChildren: () => import('./pages/forgot-password/forgot-password.module').then( m => m.ForgotPasswordPageModule)
+  },
+  {
+    path: 'password-change',
+    loadChildren: () => import('./pages/password-change/password-change.module').then( m => m.PasswordChangePageModule)
+  },
+  {
+    path: 'password-reset-otp',
+    loadChildren: () => import('./pages/password-reset-otp/password-reset-otp.module').then( m => m.PasswordResetOtpPageModule)
+  },
+  {
+    path: 'register',
+    loadChildren: () => import('./pages/register/register.module').then( m => m.RegisterPageModule)
+  },
 ];
 
 @NgModule({
