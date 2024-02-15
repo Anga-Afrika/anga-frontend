e64e0a19a0273628fdf3a4dd76de6f595b3b0a92
emuiga <muigastephen14@gmail.com>
2024-02-15 23:05:21 +0300

    register-ui

--- src/app/pages/register/register.page.ts
+++ src/app/pages/register/register.page.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.page.html',
   styleUrls: ['./register.page.scss'],
 })
-export class RegisterPage implements OnInit {
+export class RegisterPage  {
+
+  email: string | undefined;
+  password: string | undefined;
 
   constructor() { }
 
-  ngOnInit() {
-  }
+  
 
 }
