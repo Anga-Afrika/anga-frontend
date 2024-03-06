f86f326b8b0419e41edd12bb6bb23c49d8ab3e1f
emuiga <muigastephen14@gmail.com>
2024-03-06 15:08:58 +0300

    merge conflicts

--- src/app/lib/readings/readings.page.spec.ts
+++ src/app/lib/readings/readings.page.spec.ts
@@ -1,4 +1,4 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, async } from '@angular/core/testing';
 import { ReadingsPage } from './readings.page';
 
 describe('ReadingsPage', () => {
